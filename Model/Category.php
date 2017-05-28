d330209986cd83f69af234607c16df6e75f2095d
miky <mathias89100@gmail.com>
2017-05-28 23:18:43 +0200

    All

---
+++ Model/Category.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 13/04/17
+ * Time: 14:10
+ */
+
+namespace Miky\Component\Classification\Model;
+
+
+class Category implements CategoryInterface
+{
+
+}
---
+++ Model/CategoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 13/04/17
+ * Time: 16:09
+ */
+
+namespace Miky\Component\Classification\Model;
+
+
+interface CategoryInterface
+{
+
+}
